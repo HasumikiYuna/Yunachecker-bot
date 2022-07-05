1ed822ddc34cce84f444a8c2fc375dfe2e5adccd
Junaid433 <rahmanjunaid226@yahoo.com>
2022-07-05 15:54:26 +0600

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -68,7 +68,7 @@ async def chk(_, m: Message):
 @Bot.on_message(filters.command("cmds"))
 async def help(_, m: Message):
     await m.reply_text(
-        "/start - **To check bot Status**.\n/cmds - **To check bot commands.**\n/bin Query - **To check Bin\n/chk - **Stripe CHARGE\n/sk - **SK Checker"
+        "/start - **To check bot Status**.\n/cmds - **To check bot commands.**\n/bin Query - **To check Bin\n/chk - **Stripe CHARGE\n/sk - **SK Checker")
          
 @Bot.on_message(filters.command("bin"))
 async def bin(_, m: Message):
